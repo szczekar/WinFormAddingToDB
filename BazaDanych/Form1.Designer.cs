0795841bf606baa3f0b90a69abfa9f314ed71b9f
Karol Szczepaniak <szczepaniakkarol@gmail.com>
2018-05-03 21:34:38 +0200

    Add files via upload

---
+++ BazaDanych/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BazaDanych
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBaza = new System.Windows.Forms.Button();
+            this.textBoxNewUser = new System.Windows.Forms.TextBox();
+            this.buttonDopiszDoBazy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDeleteTable = new System.Windows.Forms.Button();
+            this.inputPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBaza
+            // 
+            this.buttonBaza.Location = new System.Drawing.Point(287, 223);
+            this.buttonBaza.Name = "buttonBaza";
+            this.buttonBaza.Size = new System.Drawing.Size(184, 46);
+            this.buttonBaza.TabIndex = 0;
+            this.buttonBaza.Text = "Zapisz do bazy 1 usera";
+            this.buttonBaza.UseVisualStyleBackColor = true;
+            this.buttonBaza.Click += new System.EventHandler(this.buttonBaza_Click);
+            // 
+            // textBoxNewUser
+            // 
+            this.textBoxNewUser.Location = new System.Drawing.Point(287, 87);
+            this.textBoxNewUser.Name = "textBoxNewUser";
+            this.textBoxNewUser.Size = new System.Drawing.Size(184, 20);
+            this.textBoxNewUser.TabIndex = 1;
+            // 
+            // buttonDopiszDoBazy
+            // 
+            this.buttonDopiszDoBazy.Location = new System.Drawing.Point(287, 168);
+            this.buttonDopiszDoBazy.Name = "buttonDopiszDoBazy";
+            this.buttonDopiszDoBazy.Size = new System.Drawing.Size(184, 23);
+            this.buttonDopiszDoBazy.TabIndex = 2;
+            this.buttonDopiszDoBazy.Text = "Dopisz do bazy ";
+            this.buttonDopiszDoBazy.UseVisualStyleBackColor = true;
+            this.buttonDopiszDoBazy.Click += new System.EventHandler(this.buttonDopiszDoBazy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wpisz nowego użytkownika do bazy";
+            // 
+            // buttonDeleteTable
+            // 
+            this.buttonDeleteTable.Location = new System.Drawing.Point(287, 301);
+            this.buttonDeleteTable.Name = "buttonDeleteTable";
+            this.buttonDeleteTable.Size = new System.Drawing.Size(184, 43);
+            this.buttonDeleteTable.TabIndex = 4;
+            this.buttonDeleteTable.Text = "USUŃ DANE Z TABELI";
+            this.buttonDeleteTable.UseVisualStyleBackColor = true;
+            this.buttonDeleteTable.Click += new System.EventHandler(this.buttonDeleteTable_Click);
+            // 
+            // inputPassword
+            // 
+            this.inputPassword.Location = new System.Drawing.Point(287, 131);
+            this.inputPassword.Name = "inputPassword";
+            this.inputPassword.Size = new System.Drawing.Size(184, 20);
+            this.inputPassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Wpisz hasło";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputPassword);
+            this.Controls.Add(this.buttonDeleteTable);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDopiszDoBazy);
+            this.Controls.Add(this.textBoxNewUser);
+            this.Controls.Add(this.buttonBaza);
+            this.Name = "Form1";
+            this.Text = "Zapis do bazy SQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBaza;
+        private System.Windows.Forms.TextBox textBoxNewUser;
+        private System.Windows.Forms.Button buttonDopiszDoBazy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDeleteTable;
+        private System.Windows.Forms.TextBox inputPassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BazaDanych/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BazaDanych
+{
+    public partial class Form1 : Form
+    {
+         
+        SqlCommand sqlCommand;
+        static string connectionString = "Server=KAROL;Database=TEST_CS;Trusted_Connection=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        private void openCloseDBconnection(string value)
+        {
+            using (sqlCommand = new SqlCommand(value, connection))
+            {
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void buttonBaza_Click(object sender, EventArgs e)
+        {
+            
+            string cmdText = "INSERT INTO dbo.Users (Name) VALUES ('Karol')";
+            openCloseDBconnection(cmdText);
+        }
+
+        private void buttonDopiszDoBazy_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (textBoxNewUser.Text != null)
+                {
+                    // put name into DB
+                    string inputText = textBoxNewUser.Text;
+                    string newName = string.Format("INSERT INTO dbo.Users (Name) VALUES ('{0}')",inputText);
+                    openCloseDBconnection(newName);
+                    MessageBox.Show("Imie zostało dopisane do bazy !");
+
+                    // put password into DB
+                    string password = inputPassword.Text;
+                    string newPassword = string.Format("INSERT INTO dbo.Users (Name) VALUES ('" + password + "')");
+                    openCloseDBconnection(newPassword);
+                    MessageBox.Show("Hasło zostało dopisane do bazy !");
+
+                }
+
+                else
+                {
+                    return;
+                }
+
+                
+            }
+             
+            catch(InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }         
+        }
+
+        private void buttonDeleteTable_Click(object sender, EventArgs e)
+        {
+            string deleteFromDB = "DELETE FROM dbo.Users";
+            openCloseDBconnection(deleteFromDB);
+            string setMainKey = "DBCC CHECKIDENT('dbo.Users', RESEED, 0)";
+            openCloseDBconnection(setMainKey);
+            MessageBox.Show("Wszystkie dane z tabeli usunięte !");
+
+        }
+    }
+
+}
